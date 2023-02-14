1714b51baaa7edc5b595681eae207204c1a727ed
Andy <andythomas.py@gmail.com>
2023-02-13 23:52:13 -0600

    Updated through 1:55

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,20 +1,19 @@
-import json
 from django.forms.models import model_to_dict
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
-    data = {}
-    if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.content
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-        ## Serialization
-        # model instance (model_data)
-        # turn a Python dict
-        # return JSON to my client
-
-    return JsonResponse(data)
+    """
+    DRF API View
+    """
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=400):
+        # instance = serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
+    return Response({"invalid": "not good data"}, status=400)
+    # return HttpResponse(data)
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,73 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# from django.http import Http404
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    # this method will only be called on CreateAPIView
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
+product_list_create_view = ProductListCreateAPIView.as_view()
+
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # lookup_filed = 'pk'
+
+product_detail_view = ProductDetailAPIView.as_view()
+
+
+# class ProductListAPIView(generics.ListAPIView):
+#     '''
+#     Not going to use this. Were using 1st calls view to list and add!
+#     '''
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     # lookup_filed = 'pk'
+
+# product_list_view = ProductDetailAPIView.as_view()
+
+
+@api_view(['GET', 'POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+
+    if method == "GET":
+        if pk is not None:
+            # queryset = Product.objects.filter(pk=pk)
+            # detail view
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+        
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+
+    if method == "POST":
+        # create an item
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=400):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({"invalid": "not good data"}, status=400)
